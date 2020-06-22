44093a7c5f28cc483b86e5891de2776f6526a5a3
Khadija Amjad <khadijah.amjad25@gmail.com>
2020-06-22 23:22:20 +0500

    Updated feedback controller

--- application/controllers/Feedback.php
+++ application/controllers/Feedback.php
@@ -29,8 +29,28 @@
     }
     public function sendFeedback()
     {   
+        $this->form_validation->set_rules('txtRating', 'Rating','required|trim|min_length[1]|xss_clean|greater_than[0]|less_than[5]');
+        $this->form_validation->set_rules('txtComments', 'Comments','required|trim|min_length[5]|xss_clean');
+        if ($this->form_validation->run() == FALSE) {
+            
+        } else {
+            $array = array(
+                'UID' => $this->session->userdata('UID'),
+                'Comments' => $this->input->post('txtComments'),
+                'Rating' => $this->input->post('txtRating'),
+            );
 
+            if ($this->Feedback_Model->addFeedback($array) ==true) {
+                $this->session->set_flashdata('feedbackSuccess', 'Your feedback was sent successfully!');
+                redirect("Feedback/Index");
+            } 
+            else {
+                $this->session->set_flashdata('feedbackError', 'There was an error sending your feedback. Please try again');
+                redirect("Feedback/Index");
+            }
+        }
     }
+
 }
 
 ?>
